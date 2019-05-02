18b79fe4035b7c40be4cf0eb218931e754c70cf4
Ben James <benhjames@sky.com>
2019-05-02 16:55:37 +0100

    Increase strictness of TypeScript config
    See <https://github.com/helmetjs/ienoopen/pull/5/>.

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http'
 
 export = function ienoopen () {
-  return function ienoopen (req: IncomingMessage, res: ServerResponse, next: () => void) {
+  return function ienoopen (_req: IncomingMessage, res: ServerResponse, next: () => void) {
     res.setHeader('X-Download-Options', 'noopen')
     next()
   }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
-import connect = require('connect')
-import request = require('supertest')
+import connect from 'connect'
+import request from 'supertest'
 import { IncomingMessage, ServerResponse } from 'http'
 
 import ienoopen = require('..')
@@ -8,7 +8,7 @@ describe('ienoopen', function () {
   it('sets header properly', function () {
     const app = connect()
     app.use(ienoopen())
-    app.use(function (req: IncomingMessage, res: ServerResponse) {
+    app.use(function (_req: IncomingMessage, res: ServerResponse) {
       res.setHeader('Content-Disposition', 'attachment; filename=somefile.txt')
       res.end('Download this cool file!')
     })
