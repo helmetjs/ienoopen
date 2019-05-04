49420b6ca61e3ca6c94c68d28b14b1b6d8a2db8d
Evan Hahn <me@evanhahn.com>
2019-05-04 18:12:05 -0500

    Use arrow functions in tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,11 +4,11 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 import ienoopen = require('..')
 
-describe('ienoopen', function () {
-  it('sets header properly', function () {
+describe('ienoopen', () => {
+  it('sets header properly', () => {
     const app = connect();
     app.use(ienoopen());
-    app.use(function (_req: IncomingMessage, res: ServerResponse) {
+    app.use((_req: IncomingMessage, res: ServerResponse) => {
       res.setHeader('Content-Disposition', 'attachment; filename=somefile.txt');
       res.end('Download this cool file!');
     });
@@ -17,7 +17,7 @@ describe('ienoopen', function () {
       .expect('X-Download-Options', 'noopen');
   });
 
-  it('names its function and middleware', function () {
+  it('names its function and middleware', () => {
     expect(ienoopen.name).toBe('ienoopen');
     expect(ienoopen().name).toBe('ienoopen');
   });
