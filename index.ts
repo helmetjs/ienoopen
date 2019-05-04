6f05f618a7ca9387daac46e66b389f9a5ddbe550
Evan Hahn <me@evanhahn.com>
2019-05-04 17:54:22 -0500

    Use eslint-config-helmet

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { IncomingMessage, ServerResponse } from 'http'
+import { IncomingMessage, ServerResponse } from 'http';
 
 export = function ienoopen () {
   return function ienoopen (_req: IncomingMessage, res: ServerResponse, next: () => void) {
-    res.setHeader('X-Download-Options', 'noopen')
-    next()
-  }
+    res.setHeader('X-Download-Options', 'noopen');
+    next();
+  };
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,24 +1,24 @@
-import connect from 'connect'
-import request from 'supertest'
-import { IncomingMessage, ServerResponse } from 'http'
+import connect from 'connect';
+import request from 'supertest';
+import { IncomingMessage, ServerResponse } from 'http';
 
 import ienoopen = require('..')
 
 describe('ienoopen', function () {
   it('sets header properly', function () {
-    const app = connect()
-    app.use(ienoopen())
+    const app = connect();
+    app.use(ienoopen());
     app.use(function (_req: IncomingMessage, res: ServerResponse) {
-      res.setHeader('Content-Disposition', 'attachment; filename=somefile.txt')
-      res.end('Download this cool file!')
-    })
+      res.setHeader('Content-Disposition', 'attachment; filename=somefile.txt');
+      res.end('Download this cool file!');
+    });
 
     return request(app).get('/')
-      .expect('X-Download-Options', 'noopen')
-  })
+      .expect('X-Download-Options', 'noopen');
+  });
 
   it('names its function and middleware', function () {
-    expect(ienoopen.name).toBe('ienoopen')
-    expect(ienoopen().name).toBe('ienoopen')
-  })
-})
+    expect(ienoopen.name).toBe('ienoopen');
+    expect(ienoopen().name).toBe('ienoopen');
+  });
+});
