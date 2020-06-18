16d9f913a14742a54b5d4432651027040cb7e1f8
Evan Hahn <me@evanhahn.com>
2020-06-18 10:06:40 -0500

    Mark this module as having moved to helmetjs/helmet

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { IncomingMessage, ServerResponse } from 'http';
-
-export = function ienoopen () {
-  return function ienoopen (_req: IncomingMessage, res: ServerResponse, next: () => void) {
-    res.setHeader('X-Download-Options', 'noopen');
-    next();
-  };
-}
